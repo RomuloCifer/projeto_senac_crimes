3acf499ff251ddb191131cf644da8e8478878263
Romulo <ciferomulo@gmail.com>
2026-05-23 20:56:55 -0300

    refactor: remove unused FlyToSelected function and clean up MapView component

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -1,5 +1,4 @@
-import { MapContainer, TileLayer, CircleMarker, useMap } from 'react-leaflet';
-import { useEffect } from 'react';
+import { MapContainer, TileLayer, CircleMarker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { getCategoryMeta } from '../../utils/categoryStyles';
 import MapFloatingCard from '../MapFloatingCard/MapFloatingCard';
@@ -8,25 +7,6 @@ import './MapView.css';
 
 const BRAZIL_CENTER = [-15.78, -47.93];
 const BRAZIL_ZOOM = 4;
-const CASE_ZOOM = 10;
-
-// Centraliza o mapa no caso selecionado ou volta para o Brasil ao fechar
-function FlyToSelected({ cases, selectedCaseId }) {
-  const map = useMap();
-  useEffect(() => {
-    if (!selectedCaseId) {
-      map.flyTo(BRAZIL_CENTER, BRAZIL_ZOOM, { duration: 1.4 });
-      return;
-    }
-    const found = cases.find((c) => c.id === selectedCaseId);
-    if (found) {
-      // Offset leve para cima: o pin fica na metade inferior do mapa, não no centro
-      const offsetLat = found.latitude - 0.18;
-      map.flyTo([offsetLat, found.longitude], CASE_ZOOM, { duration: 1.2 });
-    }
-  }, [selectedCaseId, cases, map]);
-  return null;
-}
 
 export default function MapView({ cases, selectedCaseId, onSelectCase, onInvestigate, onStreetView, isImmersive, onToggleImmersive, filters, activeFilter, onFilterChange, categoryCounts }) {
   const activeCase = cases.find((c) => c.id === selectedCaseId) ?? null;
@@ -49,14 +29,12 @@ export default function MapView({ cases, selectedCaseId, onSelectCase, onInvesti
           maxBounds={[[-34.0, -74.0], [5.5, -32.0]]}
           maxBoundsViscosity={1.0}
           style={{ height: mapHeight, width: '100%', borderRadius: 'inherit' }}
-          scrollWheelZoom={isImmersive}
+          scrollWheelZoom
         >
           <TileLayer
             attribution='&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
           />
-
-          <FlyToSelected cases={cases} selectedCaseId={selectedCaseId} />
 
           {cases.map((item) => {
             const meta = getCategoryMeta(item.category);
